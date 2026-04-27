fd3067e4d7de44d701bfeec378c56dd20d4b0d89
Kuraketsu <akasute8309@gmail.com>
2026-04-27 14:57:31 +0900

    pagesに合わせてpathを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
-  site: "https://kyodai-nikki.com",
+  site: "https://kyodai-nikki.github.io",
+  base: "/kyodai",
+  // site: "https://kyodai-nikki.com",
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
